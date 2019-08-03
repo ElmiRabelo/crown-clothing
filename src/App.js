189adc2bd63ab98f28c85f00b7819fb5183ed025
Elmi Rabelo <rabeloelmi@gmail.com>
2019-08-03 20:28:53 -0300

    enu-items foram atualizados para navegar com withRouterHOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Route path="/" component={HomePage} />
+		</div>
+	);
 }
 
 export default App;
