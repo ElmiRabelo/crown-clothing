e49037cc5c74e68eea608a959bda2ea2b2566403
Elmi Rabelo <rabeloelmi@gmail.com>
2019-08-06 12:56:44 -0300

    Fluxo de firebase database e firebase auth adicionados no app. Modificação no estilo de  collection-item

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
 	constructor() {
@@ -18,10 +18,26 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-			this.setState({ currentUser: user });
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+			//se o usuario existir no database, com snapshot os dados são 'puxados' para serem armazenados no state de App
+			//Case não exista em database, state de App será null.
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
 
-			console.log(user);
+				userRef.onSnapshot(snapshot => {
+					this.setState(
+						{
+							currentUser: {
+								id: snapshot.id,
+								...snapshot.data()
+							}
+						},
+						() => console.log(this.state)
+					);
+				});
+			} else {
+				this.setState({ currentUser: userAuth });
+			}
 		});
 	}
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,11 +12,41 @@ const config = {
 	appId: "1:322537233388:web:3a43844f060c5948"
 };
 
+//async function responsavel por criar um documento do user no database
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+
+	//Coletando o uid do usuario fornecido pelo google Auth e puxando os dados sobre esse documento
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+	const snapShot = await userRef.get();
+
+	//verifica se existe algum dado criado no database relacionado ao user, se não existir, cria e armazena no database
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			});
+		} catch (error) {
+			console.log("erro ao criar usuario", error.message);
+		}
+	}
+
+	return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+//Configurando google Auth para permitir loguin de usuario usando uma conta google
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
 export const SignInWithGoogle = () => auth.signInWithPopup(provider);
