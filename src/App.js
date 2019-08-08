90bfb94a8ce9e2987b27eab898c71e8b227bf953
Elmi Rabelo <rabeloelmi@gmail.com>
2019-08-08 14:22:16 -0300

    Redux adicionado, e userReducer e userActions implementados em Header e App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
 
 import "./App.css";
 
@@ -7,17 +8,16 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
+
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends React.Component {
-	constructor() {
-		super();
-		this.state = { currentUser: null };
-	}
-
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
+		const { setCurrentUser } = this.props;
+
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 			//se o usuario existir no database, com snapshot os dados são 'puxados' para serem armazenados no state de App
 			//Case não exista em database, state de App será null.
@@ -25,16 +25,14 @@ class App extends React.Component {
 				const userRef = await createUserProfileDocument(userAuth);
 
 				userRef.onSnapshot(snapshot => {
-					this.setState({
-						currentUser: {
-							id: snapshot.id,
-							...snapshot.data()
-						}
+					setCurrentUser({
+						id: snapshot.id,
+						...snapshot.data()
 					});
 				});
-			} else {
-				this.setState({ currentUser: userAuth });
 			}
+
+			setCurrentUser(userAuth);
 		});
 	}
 
@@ -45,7 +43,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
-				<Header currentUser={this.state.currentUser} />
+				<Header />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route exact path="/shop" component={ShopPage} />
@@ -56,4 +54,11 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+	setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(
+	null,
+	mapDispatchToProps
+)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
@@ -31,4 +32,10 @@ const Header = ({ currentUser }) => (
 	</div>
 );
 
-export default Header;
+// State aqui representa o Root Reducer, e atribuo o valor de currentUser para o valor definido como default em user.reducer
+const mapStateToProps = state => ({
+	currentUser: state.user.currentUser
+});
+
+//connect é um High Order Component, o que faz com que Header tenha 'conhecimento' das propriedas de Redux
+export default connect(mapStateToProps)(Header);
