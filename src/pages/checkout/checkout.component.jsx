62087a90c769fe53653b66808167f0b5c9345db4
Elmi Rabelo <rabeloelmi@gmail.com>
2019-08-13 20:09:55 -0300

    criado integração com stripe e atualizado favicon.ico

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -10,6 +10,7 @@ import {
 
 //components
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 import "./checkout.styles.scss";
 
@@ -36,6 +37,12 @@ const CheckoutPage = ({ cartItems, total }) => (
 			<CheckoutItem key={item.id} cartItem={item} />
 		))}
 		{<span className="total">TOTAL: ${total}</span>}
+		<div className="test-warning">
+			*Please use the following test credit card for payments*
+			<br />
+			4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+		</div>
+		<StripeCheckoutButton price={total} />
 	</div>
 );
 
