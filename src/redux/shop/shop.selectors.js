a716c77e303ee7a88d15d4158da41c00819512c2
Elmi Rabelo <rabeloelmi@gmail.com>
2019-08-13 18:08:09 -0300

    atualização em collection-overview, e shop.selector, criado selector para converter os dados de shop em array e utiliza-los em collection-overview

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,6 +7,13 @@ export const selectCollections = createSelector(
 	shop => shop.collections
 );
 
+//collections é um objeto, com isso converto para array, vou então pecorrer esse array com map. e para cada item irei pegar os valores de seu index especifico.
+// Ex: collections[hats]
+export const selectCollectionsForPreview = createSelector(
+	[selectCollections],
+	collections => Object.keys(collections).map(key => collections[key])
+);
+
 export const selectCollection = collectionUrlParam =>
 	createSelector(
 		[selectCollections],
