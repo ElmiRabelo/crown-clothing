90e07d8047e3d76ec1e73e6e6bf9bea3cf487bff
Elmi Rabelo <rabeloelmi@gmail.com>
2019-08-14 11:50:49 -0300

    alteração no README.md e removido minddlewares no deploy

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-const minddlewares = [logger];
+const minddlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+	minddlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...minddlewares));
 
