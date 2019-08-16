6a3be12ba244e0728499ffac6ac660b5ad4938bd
Elmi Rabelo <rabeloelmi@gmail.com>
2019-08-16 18:38:51 -0300

    Custom component convertido para utilizar CSS-in-JS

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,22 +1,10 @@
 import React from "react";
 
-import "./custom-button.styles.scss";
+import { CustomButtonContainer } from "./custom-button.styles";
 
 //Sobre isGoogleSignIn: se a propriedade foi passada como prop então será true, o que vai aplicar a classe unica de google=sign-in. Se a prop não foi passada é false, a classe não se aplica.
-const CustomButton = ({
-	children,
-	isGoogleSignIn,
-	inverted,
-	...otherProps
-}) => (
-	<button
-		className={`${inverted ? "inverted" : ""} ${
-			isGoogleSignIn ? "google-sign-in" : ""
-		} custom-button`}
-		{...otherProps}
-	>
-		{children}
-	</button>
+const CustomButton = ({ children, ...props }) => (
+	<CustomButtonContainer {...props}>{children}</CustomButtonContainer>
 );
 
 export default CustomButton;
